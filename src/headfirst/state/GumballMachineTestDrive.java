7b297f34a320abc4ac5a65c2d1900a1106269618
강수빈/과제는 j1_201511078에 있습니다! <01031342541@naver.com>
2016-05-30 17:47:25 +0900

    w14

---
+++ src/headfirst/state/GumballMachineTestDrive.java
@@ -0,0 +1,10 @@
+package headfirst.state;
+
+public class GumballMachineTestDrive{
+  public static void main(String[] args){
+    GumballMachine gm=new GumballMachine(5);
+    gm.insertQuarter();
+    gm.insertQuarter();
+    gm.turnCrank();
+  }
+}
